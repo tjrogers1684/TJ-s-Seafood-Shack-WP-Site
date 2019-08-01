2f13ab07edd40a4d8e33b2f96e318b103bdef9b9
TJ Rogers <tj.rogers7@gmail.com>
2019-08-01 16:07:53 -0400

    - Get Menu on Menu page   - Broken up by Categories

--- page-17.php
+++ page-17.php
@@ -6,43 +6,6 @@ get_header();
 
 ?>
 
-<?php $menu_types = get_terms('menu_item_type', array('hide_empty' => 0, 'parent' =>0)); foreach($menu_types as $menu_type) : ?>
-
-	<?php
-
-		$term_slug = $menu_type->slug;
-
-		//echo '<pre>'. print_r( $menu_type, true) .'</pre>';
-		echo '<p>'. $term_slug .'</p>';
-
-		// ----- STEM ARTICLES QUERY ----
-	$menu_items_listing_args = [
-		'post_type' => 'menu_item',
-		'orderby'	=> 'title',
-		'order'		=> 'ASC',
-		'posts_per_page' => 600,
-		'tax_query' => array(
-	        array(
-	            'taxonomy' => 'menu_item_type',
-	            'field'    => 'slug',
-	            'terms'    => $term_slug,
-	            'compare' => '=',
-	        ),
-	    ),
-	];
-
-	// The Query
-	$menu_items_listing_query = new WP_Query( $menu_items_listing_args );
-
-	?>
-
-<?php
-	endforeach;
-?>
-
-
-
-
 <?php if( isset($_GET['pid']) ){ $posttype = get_post( $_GET['pid'] )->post_type; } ?>
 
 <div class="content-wrap">
@@ -62,33 +25,58 @@ get_header();
 			<div class="academic-links-listing-container">
 
 				<div class="academic-links-listing">
-
-					<?php if ( $menu_items_listing_query->have_posts() ) : while ($menu_items_listing_query->have_posts() ) : $menu_items_listing_query->the_post(); ?>
+					<?php $menu_types = get_terms('menu_item_type', array('hide_empty' => 0, 'parent' =>0)); foreach($menu_types as $menu_type) : ?>
 
 						<?php
-							$title  = the_title('','',false);
-							$post_meta = get_post_meta( $post->ID );
-							//$link_url = get_field('link_url');
 
-							echo $link_url;
-							echo '<pre>'.print_r($post_meta, true).'</pre>';
-							$categories = wp_get_object_terms( $post->ID, 'resource_link_categories' );
-							echo '<pre>'.print_r($categories, true).'</pre>';
+							$term_slug = $menu_type->slug;
 
-							// foreach ( $categories as $category ) {
-						 //        echo '<span class="category-item">'.$category->name;
-						 //    }
+							//echo '<pre>'. print_r( $menu_type, true) .'</pre>';
+							echo '<p>'. $term_slug .'</p>';
+
+							// ----- STEM ARTICLES QUERY ----
+							// $menu_items_listing_args = [
+							// 	'post_type' => 'menu_item',
+							// 	'orderby'	=> 'title',
+							// 	'order'		=> 'ASC',
+							// 	'posts_per_page' => 600,
+							// 	'tax_query' => array(
+							//         array(
+							//             'taxonomy' => 'menu_item_type',
+							//             'field'    => 'slug',
+							//             'terms'    => $term_slug,
+							//         ),
+							//     ),
+							// ];
+
+							$post_args = array(
+								'posts_per_page' => 50,
+								'post_type' => 'menu_item', // you can change it according to your custom post type
+								'tax_query' => array(
+									array(
+										'taxonomy' => 'menu_item_type', // you can change it according to your taxonomy
+										'field' => 'slug', // this can be 'term_id', 'slug' & 'name'
+										'terms' => $term_slug,
+									)
+								)
+							);
+
+							// The Query
+							//$menu_items_listing_query = new WP_Query( $menu_items_listing_args );
+
+							$myposts = get_posts($post_args);
+
 						?>
 
+					<?php foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
 						<div class="link-item">
 							<h2 class="link-item-title"><?php the_title(); ?></h2>
 						</div>
-
-					<?php endwhile; ?>
+					<?php endforeach; // Term Post foreach ?>
 
 					<?php wp_reset_postdata(); ?>
 
-					<?php endif; ?>
+					<?php endforeach; // End Term foreach; ?> 
 
 				</div><!-- END MEMBERS LISTING -->
 			</div><!-- END MEMBERS LISTING CONTAINER -->
