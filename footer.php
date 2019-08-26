429acc3761d821aa305abbe52205dc7c55026ed8
TJ Rogers <tj.rogers7@gmail.com>
2019-08-26 10:48:57 -0400

    - Switch footer form from Ninja Forms to MailChimp embed

--- footer.php
+++ footer.php
@@ -84,7 +84,27 @@
 		<div class="footer-section footer-contact">
 
 			<div class="newsletter-signup">
-				<?php Ninja_Forms()->display( 3 ); ?>
+				<!-- Begin Mailchimp Signup Form -->
+					<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+					<style type="text/css">
+						#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+						/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+						   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+					</style>
+
+					<div id="mc_embed_signup">
+						<form action="https://tjsseafoodshack.us3.list-manage.com/subscribe/post?u=dab25526378f73de0651c4030&amp;id=d81b2822f8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						    <div id="mc_embed_signup_scroll">
+								<label for="mce-EMAIL">Sign up for news, updates, and offers:</label>
+								<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+							    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_dab25526378f73de0651c4030_d81b2822f8" tabindex="-1" value=""></div>
+							    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+						    </div>
+						</form>
+					</div>
+
+				<!--End mc_embed_signup-->
 			</div>
 
 			<div class="contact-info-hours">
